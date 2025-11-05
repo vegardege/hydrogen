01c4b926bc886ccf18f9de614ba023d2243f570c
Vegard Egeland <vegardegeland@gmail.com>
2025-11-05 13:50:58 +0100

    Add black box monitoring to `prometheus` setup

--- deploy/deploy-monitoring.sh
+++ deploy/deploy-monitoring.sh
@@ -21,19 +21,23 @@ fi
 echo "Uploading monitoring configuration to server..."
 scp -i ~/.ssh/id_ed25519_hydro "$MONITORING_DIR/compose.yml" "hydro@$SERVER_IP:/tmp/monitoring-compose.yml"
 scp -i ~/.ssh/id_ed25519_hydro "$MONITORING_DIR/prometheus/prometheus.yml" "hydro@$SERVER_IP:/tmp/prometheus.yml"
+scp -i ~/.ssh/id_ed25519_hydro "$MONITORING_DIR/blackbox/blackbox.yml" "hydro@$SERVER_IP:/tmp/blackbox.yml"
 
 echo "Setting up directories and deploying monitoring stack..."
 ssh -i ~/.ssh/id_ed25519_hydro "hydro@$SERVER_IP" << 'EOF'
     set -e
     echo "Creating /srv/monitoring directory structure..."
     sudo mkdir -p /srv/monitoring/prometheus
+    sudo mkdir -p /srv/monitoring/blackbox
     sudo chown -R hydro:hydro /srv/monitoring
 
     echo "Moving configuration files..."
     mv /tmp/monitoring-compose.yml /srv/monitoring/compose.yml
     mv /tmp/prometheus.yml /srv/monitoring/prometheus/prometheus.yml
+    mv /tmp/blackbox.yml /srv/monitoring/blackbox/blackbox.yml
     chmod 644 /srv/monitoring/compose.yml
     chmod 644 /srv/monitoring/prometheus/prometheus.yml
+    chmod 644 /srv/monitoring/blackbox/blackbox.yml
 
     echo "Starting monitoring stack..."
     cd /srv/monitoring
